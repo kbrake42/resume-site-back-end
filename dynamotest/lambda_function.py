2ece2ba2d8b155a1c613cd5fe4f03358e6f08e28
Ken <kbrake42@gmail.com>
2022-10-04 22:19:01 -0400

    Got reading from and updating dynamodb working

--- dynamotest/lambda_function.py
+++ dynamotest/lambda_function.py
@@ -5,6 +5,12 @@ from boto3.dynamodb.conditions import Key, Attr
 #always start with the lambda_handler
 def lambda_handler(event, context):
 
+    #Get the count from the database item
+    def getCount(dbItem):
+        json_tree = json.loads(dbItem)
+        item = json_tree['Item']
+        return (item['count'])
+
     # make the connection to dynamodb
     dynamodb = boto3.resource('dynamodb')
 
@@ -12,18 +18,28 @@ def lambda_handler(event, context):
     table = dynamodb.Table("resume-site-table")
 
     # get item from database
-    items = table.get_item(Key={"visitors": 'resume'})
+    items = json.dumps(table.get_item(Key={"visitors": 'resume'}))
     
-    print(json.dumps(items))
+    count = getCount(items)
+    incrementedCount = str(int(count) + 1)
+    print("IncrementedCount= " + incrementedCount)
     
-    
-    #response = table.update_item(
-    #    Key={'visitors': 'resume'},
-    #    UpdateExpression="set item.count = item.count + 1",
-    #    #ExpressionAttributeValues={':val': Decimal(str('1'))},
-    #    ReturnValues="UPDATED_NEW")
+    response = table.update_item(
+        Key={'visitors': "resume"},
+        UpdateExpression="SET #count = :c",
+        ExpressionAttributeNames= {'#count':'count'},
+        ExpressionAttributeValues={':c':incrementedCount},
+        ReturnValues="UPDATED_NEW"
+        )
 
-    new_num = int('120') + 1
-    new_num = str(new_num)
-    print(new_num)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Visitor ": incrementedCount
+        })
+    }
+    
     
