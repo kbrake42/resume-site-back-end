044d207f675dce8d86a58959707639296399bd99
Ken <kbrake42@gmail.com>
2022-10-04 22:24:25 -0400

    Fixed Typo

--- dynamotest/lambda_function.py
+++ dynamotest/lambda_function.py
@@ -38,7 +38,7 @@ def lambda_handler(event, context):
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "Visitor ": incrementedCount
+            "Visitor": incrementedCount
         })
     }
     
