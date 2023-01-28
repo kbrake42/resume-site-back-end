472eca5aacc7136d017d2b8c071174bde357f239
Ken <kbrake42@gmail.com>
2023-01-27 19:51:55 -0500

    Updated Lambda

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ from boto3.dynamodb.conditions import Key, Attr
 #always start with the lambda_handler
 def lambda_handler(event, context):
 
-    #Get the count from the database item
+    #Get the count from the database item--updated
     def getCount(dbItem):
         json_tree = json.loads(dbItem)
         item = json_tree['Item']
